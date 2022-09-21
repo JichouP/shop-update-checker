57ce79ef0d2b838c95a2aba8e6aac75ad812b65c
JichouP <akkunn100@gmail.com>
2022-09-21 13:53:20 +0900

    :sparkles: impl Display for ShopKind

--- src/domain/shop.rs
+++ src/domain/shop.rs
@@ -1,7 +1,28 @@
+use std::fmt::Display;
+
 #[derive(Clone, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
 pub enum ShopKind {
     Ark,
+    PCKoubou,
     Sofmap,
     Tsukumo,
-    PCKoubou,
+}
+
+impl Display for ShopKind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Ark => {
+                write!(f, "Ark")
+            }
+            Self::PCKoubou => {
+                write!(f, "PCKoubou")
+            }
+            Self::Sofmap => {
+                write!(f, "Sofmap")
+            }
+            Self::Tsukumo => {
+                write!(f, "Tsukumo")
+            }
+        }
+    }
 }
